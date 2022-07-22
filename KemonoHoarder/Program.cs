1068f1c7ed99bd50a853b21acbe0e67e9376a98c
TheRealKamisama <TheRealKamisama@163.com>
2022-07-22 13:22:39 +0800

    信誉起来了 打个郊县

--- KemonoHoarder/Program.cs
+++ KemonoHoarder/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using GammaLibrary.Extensions;
@@ -54,19 +56,35 @@ namespace KemonoHoarder
             Console.WriteLine($"已找到作品: {posts.Count} 个");
             const string data = "https://kemono.party/data";
             var downloads = new List<FileToDownload>();
+            var linkParser = new Regex(@"http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var embededs = new List<string>();
             foreach (var post in posts)
             {
                 var folder =
                     $"[{post.Published.Year}-{post.Published.Month}-{post.Published.Day}] [{post.Id}] {post.Title.RemoveIllegalChars()}".Trim();
-                var urls = "";
+                var urls = new StringBuilder();
                 Directory.CreateDirectory(Path.Combine(path, folder));
                 foreach (var attachment in post.Attachments)
                 {
                     downloads.Add(new FileToDownload{ path = Path.Combine(path, folder), name = attachment.Name, url = data + attachment.Path});
-                    urls += $"{Environment.NewLine}{attachment.Name},{data}{attachment.Path}";
+                    urls.AppendLine($"{attachment.Name},{data}{attachment.Path}");
                 }
-                await System.IO.File.WriteAllTextAsync(Path.Combine(path, folder, "urls.txt"), urls);
+
+                var embeded = linkParser.Matches(post.Content);
+                embededs.AddRange(embeded.Select(e => e.Value));
+                await System.IO.File.WriteAllTextAsync(Path.Combine(path, folder, "urls.txt"), urls.ToString());
+                await System.IO.File.WriteAllTextAsync(Path.Combine(path, folder, "content.txt"), post.Content);
+                await System.IO.File.WriteAllLinesAsync(Path.Combine(path, folder, "contenturls.txt"),
+                    embeded.Select(e => e.Value));
+                await System.IO.File.WriteAllTextAsync(Path.Combine(path, folder, "embeded.txt"),
+                    $"[{post.Embed.Subject}][{post.Embed.Description}],{post.Embed.Url}");
+                Console.WriteLine($"作品: {post.Title} 附件: {post.Attachments.Count} 件 内含链接: {embeded.Count} 个");
             }
+            Console.WriteLine("附件链接已写入 urls.txt, 内容已写入: content.txt, 内容内的链接已写入 contenturls.txt, 嵌入内容已写入: embeded.txt.");
+            embededs = embededs.Distinct().ToList();
+            await System.IO.File.WriteAllLinesAsync(Path.Combine(path, "all_embededurls.txt"), embededs);
+            Console.WriteLine("所有嵌入在内容里的链接已写入: all_embededurls.txt");
+            Console.WriteLine("所有找到的链接");
             Console.WriteLine($"已找到附件: {downloads.Count} 个");
             Console.WriteLine($"正在下载附件···");
             DownloadPics(downloads).Wait();
@@ -82,7 +100,7 @@ namespace KemonoHoarder
         {
             var proxy = new WebProxy
             {
-                Address = new Uri($"http://localhost:7890"),
+                Address = new Uri($"http://192.168.0.104:7890"),
                 BypassProxyOnLocal = false,
                 UseDefaultCredentials = true
             };
@@ -90,7 +108,7 @@ namespace KemonoHoarder
             {
                 Proxy = proxy,
             };
-            return  new HttpClient(handler: httpClientHandler, disposeHandler: true);
+            return  new HttpClient(handler: new RetryHandler(httpClientHandler), disposeHandler: true);
         }
         private static volatile int count = 1;
         static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(50);
@@ -133,6 +151,34 @@ namespace KemonoHoarder
             }
 
             return result;
+        }
+    }
+    public class RetryHandler : DelegatingHandler
+    {
+        // Strongly consider limiting the number of retries - "retry forever" is
+        // probably not the most user friendly way you could respond to "the
+        // network cable got pulled out."
+        private const int MaxRetries = 5;
+
+        public RetryHandler(HttpMessageHandler innerHandler) : base(innerHandler)
+        {
+
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response = null;
+            for (int i = 0; i < MaxRetries; i++)
+            {
+                response = await base.SendAsync(request, cancellationToken);
+                if (response.IsSuccessStatusCode) {
+                    return response;
+                }
+            }
+
+            return response;
         }
     }
 }
